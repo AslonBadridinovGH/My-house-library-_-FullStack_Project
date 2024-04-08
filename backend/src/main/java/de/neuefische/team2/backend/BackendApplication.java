54f8e0ecad15838af6c7b8b6dd1b9de23638d674
AslonBadridinovGH <151860764+AslonBadridinovGH@users.noreply.github.com>
2024-04-08 03:54:05 +0200

    changed

--- backend/src/main/java/de/neuefische/team2/backend/BackendApplication.java
+++ backend/src/main/java/de/neuefische/team2/backend/BackendApplication.java
@@ -11,8 +11,10 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-@Bean
+
+	@Bean
 	public Cloudinary cloudinaryBean(){
 		return new Cloudinary();
 }
+
 }
